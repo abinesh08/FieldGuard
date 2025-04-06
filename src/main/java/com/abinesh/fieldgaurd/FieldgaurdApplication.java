f984334cb93e57bf2d2feef05acef52ef74ac6aa
Abinesh <abinesh.r.2019.cse@ritchennai.edu.in>
2025-04-06 18:42:32 +0530

    commit

---
+++ src/main/java/com/abinesh/fieldgaurd/FieldgaurdApplication.java
@@ -0,0 +1,13 @@
+package com.abinesh.fieldgaurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldgaurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldgaurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abinesh/fieldgaurd/FieldgaurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abinesh.fieldgaurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldgaurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
