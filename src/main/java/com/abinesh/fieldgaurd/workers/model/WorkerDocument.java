4add27c48cba2597975b1e6893e2924002aadd2c
Abinesh <abinesh.r.2019.cse@ritchennai.edu.in>
2025-04-16 18:48:45 +0530

    commit WorkerDocument

---
+++ src/main/java/com/abinesh/fieldgaurd/workers/model/WorkerDocument.java
@@ -0,0 +1,28 @@
+package com.abinesh.fieldgaurd.workers.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WorkerDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String documentType;
+
+    @Lob
+    @Column(name= "document_file", columnDefinition = "LONGBLOB")
+    private byte[] documentFile;
+    private String fileName;
+    private String fileType;
+
+    @ManyToOne
+    @JoinColumn(name="worker_id")
+    private Worker worker;
+
+}
